32465efd2a47c3a04f8e192890ff01445d127ca5
cemolcay <ccemolcay@gmail.com>
2015-10-25 23:15:23 -0700

    Demo added

--- 3DTouchHelper/Example/ViewController.swift
+++ 3DTouchHelper/Example/ViewController.swift
@@ -28,12 +28,80 @@ extension UIGestureRecognizerState: CustomDebugStringConvertible {
 }
 
 class ViewController: UIViewController {
+    var forceLayers = [ForceLayer]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //basicExample()
+        layerExample()
+    }
+}
+
+extension ViewController { // Basic Example
+    func basicExample() {
         add3DTouchGestureRecognizer { (touchIndex, state, force, normalizedForce, forceValue, location) in
             print("touch \(touchIndex) \(state) \(forceValue) value \(normalizedForce) at \(location)")
         }
     }
 }
 
+extension ViewController { // Layer Example
+    func layerExample() {
+        add3DTouchGestureRecognizer { (touchIndex, state, force, normalizedForce, touchForce, location) -> Void in
+            if state == .Began {
+                self.addLayer(touchIndex, position: location, normalizedForce: normalizedForce)
+            } else if state == .Changed {
+                self.updateLayer(touchIndex, position: location, normalizedForce: normalizedForce)
+            } else if state == .Ended || state == .Cancelled {
+                self.removeLayerAtIndex(touchIndex)
+            }
+        }
+    }
+
+    func addLayer(index: Int, position: CGPoint, normalizedForce: CGFloat) {
+        let forceLayer = ForceLayer(position: position, normalizedForce: normalizedForce)
+        forceLayers.append(forceLayer)
+        view.layer.addSublayer(forceLayer)
+    }
+
+    func updateLayer(index: Int, position: CGPoint, normalizedForce: CGFloat) {
+        let forceLayer = forceLayers[index]
+        forceLayer.update(position, normalizedForce: normalizedForce)
+    }
+
+    func removeLayerAtIndex(index: Int) {
+        let forceLayer = forceLayers[index]
+        forceLayer.removeFromSuperlayer()
+        forceLayers.removeAtIndex(index)
+    }
+}
+
+class ForceLayer: CALayer {
+    var maxRadius: CGFloat = 300
+
+    init (position: CGPoint, normalizedForce: CGFloat) {
+        super.init()
+        self.borderColor = UIColor.blackColor().CGColor
+        self.borderWidth = 0.5
+        self.update(position, normalizedForce: normalizedForce)
+    }
+
+    override init(layer: AnyObject) {
+        super.init(layer: layer)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    func update (position: CGPoint, normalizedForce: CGFloat) {
+        let radius = getRadius(normalizedForce)
+        self.frame = CGRectMake(0, 0, radius, radius)
+        self.position = position
+        self.cornerRadius = self.frame.size.width/2
+    }
+
+    private func getRadius(normalizedForce: CGFloat) -> CGFloat {
+        return maxRadius * normalizedForce
+    }
+}
