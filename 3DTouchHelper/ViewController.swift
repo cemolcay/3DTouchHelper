577f6b6f5cab1258ed3916bcccd5381daf74dd01
cemolcay <ccemolcay@gmail.com>
2015-10-25 21:35:18 -0700

    Initial Commit

---
+++ 3DTouchHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DTouchHelper
+//
+//  Created by Cem Olcay on 25/10/15.
+//  Copyright © 2015 prototapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
