47ecd1c9c2cf9a732887ed652580d93f77c33674
Alex Taffe <alex.taffe@gmail.com>
2016-12-29 17:17:25 -0500

    Fix access issues

--- 3DTouchHelper/3DTouchHelper/TDTouchGestureRecognizer.swift
+++ 3DTouchHelper/3DTouchHelper/TDTouchGestureRecognizer.swift
@@ -9,38 +9,38 @@
 import UIKit
 import UIKit.UIGestureRecognizerSubclass
 
-extension UIView {
-
-    func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback) {
+public extension UIView {
+    
+    public func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback) {
         let gesture = TDTouchGestureRecognizer(callback: callback)
         addGestureRecognizer(gesture)
     }
-
-    func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback, forceValue: TDTouchForceValue) {
+    
+    public func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback, forceValue: TDTouchForceValue) {
         let gesture = TDTouchGestureRecognizer(callback: callback, forceValue: forceValue)
         addGestureRecognizer(gesture)
     }
 }
 
-extension UIViewController {
-
-    func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback) {
+public extension UIViewController {
+    
+    public func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback) {
         view.add3DTouchGestureRecognizer(callback)
     }
-
-    func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback, forceValue: TDTouchForceValue) {
+    
+    public func add3DTouchGestureRecognizer(_ callback: @escaping TDTouchGestureRecognizerCallback, forceValue: TDTouchForceValue) {
         view.add3DTouchGestureRecognizer(callback, forceValue: forceValue)
     }
 }
 
 // MARK: - 3DTouch
 
-extension UITouch {
-    var normalizedForce: CGFloat {
+public extension UITouch {
+    public var normalizedForce: CGFloat {
         return force / maximumPossibleForce
     }
-
-    func forceValue(_ value: TDTouchForceValue) -> TDTouchForce {
+    
+    public func forceValue(_ value: TDTouchForceValue) -> TDTouchForce {
         if normalizedForce <= value.Low {
             return .Low
         } else if normalizedForce > value.Low && normalizedForce <= value.Mid {
@@ -53,28 +53,28 @@ extension UITouch {
     }
 }
 
-enum TDTouchForce: String, CustomDebugStringConvertible {
+public enum TDTouchForce: String, CustomDebugStringConvertible {
     case Low = "Low"
     case Mid = "Mid"
     case High = "High"
-
-    internal var debugDescription: String {
+    
+    public var debugDescription: String {
         return rawValue
     }
 }
 
-struct TDTouchForceValue {
-    var Low: CGFloat
-    var Mid: CGFloat
-    var High: CGFloat
-
-    init() {
+public struct TDTouchForceValue {
+    public var Low: CGFloat
+    public var Mid: CGFloat
+    public var High: CGFloat
+    
+    public init() {
         Low = 0.3
         Mid = 0.6
         High = 1
     }
-
-    init(Low: CGFloat, Mid: CGFloat, High: CGFloat) {
+    
+    public init(Low: CGFloat, Mid: CGFloat, High: CGFloat) {
         self.Low = Low
         self.Mid = Mid
         self.High = High
@@ -83,7 +83,7 @@ struct TDTouchForceValue {
 
 // MARK: - Callback
 
-typealias TDTouchGestureRecognizerCallback = (
+public typealias TDTouchGestureRecognizerCallback = (
     _ touchIndex: Int,
     _ state: UIGestureRecognizerState,
     _ force: CGFloat,
@@ -93,53 +93,53 @@ typealias TDTouchGestureRecognizerCallback = (
 
 // MARK: - Recognizer
 
-private class TDTouchGestureRecognizer: UIGestureRecognizer {
-
+public class TDTouchGestureRecognizer: UIGestureRecognizer {
+    
     // MARK: Properties
-
+    
     fileprivate var callback: TDTouchGestureRecognizerCallback?
     fileprivate var forceValue: TDTouchForceValue!
-
+    
     // MARK: Init
-
+    
     convenience init(callback: @escaping TDTouchGestureRecognizerCallback, forceValue: TDTouchForceValue = TDTouchForceValue()) {
         self.init(target: nil, action: Selector(""))
         self.callback = callback
         self.forceValue = forceValue
     }
-
+    
     override init(target: Any?, action: Selector?) {
         super.init(target: target, action: action)
     }
-
+    
     // MARK: Touches
-
-    fileprivate override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
         super.touchesBegan(touches, with: event)
         self.state = .began
         handleGesture(touches)
     }
-
-    fileprivate override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
         super.touchesMoved(touches, with: event)
         self.state = .changed
         handleGesture(touches)
     }
-
-    fileprivate override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
         super.touchesEnded(touches, with: event)
         self.state = .ended
         handleGesture(touches)
     }
-
-    fileprivate override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+    
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
         super.touchesCancelled(touches, with: event)
         self.state = .cancelled
         handleGesture(touches)
     }
-
+    
     // MARK: Handler
-
+    
     fileprivate func handleGesture(_ touches: Set<UITouch>) {
         for (index, touch) in touches.enumerated() {
             callback?(
